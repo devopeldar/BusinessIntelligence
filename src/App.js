5b7672c980814ce848ec1a5cd974f29cf7042dc9
Sebastian Bustos <sebastian28273@hotmail.com>
2023-12-04 16:37:40 -0300

    PrimerCommit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Login from './components/auth/Login'; // Ruta relativa al componente Login.js
+import Login from './components/authentication/Login'; // Ruta relativa al componente Login.js
 
 const App = () => {
   return (
     <div>
-      {/* Otros componentes de tu aplicación */}
-      <Login /> {/* Utiliza el componente de inicio de sesión aquí */}
+      {/* Otros componentes de tdddddu aplicación */}
+      <Login /> {/* Utiliza el componente de iniciosssss de sesión aquí */}
     </div>
   );
 };
