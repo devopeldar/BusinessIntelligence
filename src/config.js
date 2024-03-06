c5884bda420d8aa62a3b91c0d033541c855c2112
Sebastian Bustos <sebastian28273@hotmail.com>
2024-03-06 15:33:04 -0300

    V2024 Seba Cambio URl para produccion

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //URL SITIO DEL BACKEND
  //const API_URL = 'http://200.115.185.55:44474'; // Reemplaza con la URL de tu API
-const API_URL = 'http://localhost:5052'; // Reemplaza con la URL de tu API
-
+//const API_URL = 'http://localhost:5052'; // Reemplaza con la URL de tu API
+const API_URL = 'http://190.15.192.88:2024'; 
 export default API_URL;
