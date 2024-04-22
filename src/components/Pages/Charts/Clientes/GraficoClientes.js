3c038242be99d4d4ee9841ff3658c7b8377de878
Sebastian Bustos <sebastian28273@hotmail.com>
2024-04-22 13:50:45 -0300

    V20240422 Charts Clientes

--- src/components/Pages/Charts/Clientes/GraficoClientes.js
+++ src/components/Pages/Charts/Clientes/GraficoClientes.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, RadialBarChart, RadialBar } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, RadialBarChart, RadialBar, AreaChart, Area, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar, RadarChart } from 'recharts';
 import MDTypography from '../../../controls/MDTypography';
 import MDBox from '../../../controls/MDBox';
-import { Card } from '@mui/material';
+import { Card, Grid } from '@mui/material';
 import BasicLayout from '../../../layauots/BasicLayout';
 import bgImage from "../../../../assets/images/bg-sign-up-cover.jpeg";
-const data = [
+import {
+  GaugeContainer,
+  GaugeValueArc,
+  GaugeReferenceArc,
+  useGaugeState,
+} from '@mui/x-charts/Gauge';
+import { Gauge, gaugeClasses } from '@mui/x-charts/Gauge';
+import { PieChart, pieArcLabelClasses } from '@mui/x-charts/PieChart';
+import PageLayout from '../../../layauots/PageLayout';
+import { red } from '@mui/material/colors';
+const data2 = [
   { name: 'Enero', ventas: 400 },
   { name: 'Febrero', ventas: 300 },
   { name: 'Marzo', ventas: 600 },
   { name: 'Abril', ventas: 200 },
   { name: 'Mayo', ventas: 700 },
+  { name: 'Junio', ventas: 400 },
+  { name: 'Julio', ventas: 300 },
+  { name: 'Agosto', ventas: 600 },
+  { name: 'Septiembre', ventas: 200 },
+  { name: 'Octubre', ventas: 700 },
 ];
 
-
+const data = [
+  { id: 0, value: 10, label: 'series A' },
+  { id: 1, value: 15, label: 'series B' },
+  { id: 2, value: 20, label: 'series C' },
+];
 const dataPie = [
     { name: '18-24', uv: 31.47, pv: 2400, fill: '#8884d8' },
     { name: '25-29', uv: 26.69, pv: 4567, fill: '#83a6ed' },
@@ -32,50 +51,136 @@ const dataPie = [
   };
 
 
+  function GaugePointer() {
+    const { valueAngle, outerRadius, cx, cy } = useGaugeState();
+  
+    if (valueAngle === null) {
+      // No value to display
+      return null;
+    }
+  
+    const target = {
+      x: cx + outerRadius * Math.sin(valueAngle),
+      y: cy - outerRadius * Math.cos(valueAngle),
+    };
+    return (
+      <g>
+        <circle cx={cx} cy={cy} r={5} fill="red" />
+        <path
+          d={`M ${cx} ${cy} L ${target.x} ${target.y}`}
+          stroke="red"
+          strokeWidth={3}
+        />
+      </g>
+    );
+  }
+
+  const settings = {
+    width: 200,
+    height: 200,
+    value: 60,
+  };
+  
+  const dataarea = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+  
+
+  const dataradar = [
+    {
+      subject: 'Math',
+      A: 120,
+      B: 110,
+      fullMark: 150,
+    },
+    {
+      subject: 'Chinese',
+      A: 98,
+      B: 130,
+      fullMark: 150,
+    },
+    {
+      subject: 'English',
+      A: 86,
+      B: 130,
+      fullMark: 150,
+    },
+    {
+      subject: 'Geography',
+      A: 99,
+      B: 100,
+      fullMark: 150,
+    },
+    {
+      subject: 'Physics',
+      A: 85,
+      B: 90,
+      fullMark: 150,
+    },
+    {
+      subject: 'History',
+      A: 65,
+      B: 85,
+      fullMark: 150,
+    },
+  ];
+
+
+  const size = {
+    width: 400,
+    height: 200,
+  };
 const GraficoClientes = () => {
   return (
-    // <BasicLayout image={bgImage}>
-    //   <Card>
-    //     <MDBox
-    //       variant="gradient"
-    //       bgColor="warning"
-    //       borderRadius="lg"
-    //       coloredShadow="success"
-    //       mx={2}
-    //       mt={-3}
-    //       p={3}
-    //       mb={1}
-    //       textAlign="center"
-    //     >
-    //       <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-    //         Ejemplos de Gráficos de Clientes
-    //       </MDTypography>
-    //       <MDTypography  display="block" variant="button" color="white" my={1}>
-    //         Prueba de cómo se vería un gráfico
-    //       </MDTypography>
-    //     </MDBox>
-    //     <ResponsiveContainer width="100%" height={800}>
-    //         <BarChart width={200} height={300} data={data}>
-    //         <CartesianGrid strokeDasharray="3 3" />
-    //         <XAxis dataKey="name" />
-    //         <YAxis />
-    //         <Tooltip />
-    //         <Legend />
-    //         <Bar dataKey="ventas" fill="#8884d8" />
-    //         </BarChart>
-
-    //         <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={dataPie}>
-    //         <RadialBar minAngle={15} label={{ position: 'insideStart', fill: '#fff' }} background clockWise dataKey="uv" />
-    //         <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={legendStyle} />
-    //       </RadialBarChart>
-
-
-    //     </ResponsiveContainer>
-    //   </Card>
-    // </BasicLayout>
-    <BasicLayout image={bgImage}>
-    {/* <div style={{ background: 'white',  width:'850px', padding: '20px' }}> */}
-      <MDBox
+   
+    <PageLayout style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      {/* <MDBox px={1} width="100%" height="100vh" mx="auto"> */}
+      <MDBox width="100%" height="100%">
+      <Grid  width='80%' justifyContent="center" alignItems="center" height="100%">
+          {/* <Grid item xs={11} sm={9} md={5} lg={4} xl={3}> */}
+          <Card style={{ width: '100%', marginLeft:'260px', marginTop:'60px',   marginRight: 'auto' }}>
+      <MDBox 
         variant="gradient"
         bgColor="warning"
         borderRadius="lg"
@@ -93,33 +198,88 @@ const GraficoClientes = () => {
           Prueba de cómo se verían los gráficos
         </MDTypography>
       </MDBox>
-      <div style={{ display: 'flex', width:'800px', justifyContent: 'space-between' }}>
+      <div style={{ display: 'flex', marginTop: '15px', justifyContent: 'space-between', width: '100%' }}>
         {/* Primer Gráfico */}
-        <Card style={{ width: '48%' }}>
-          <ResponsiveContainer width="100%" height={300}>
-            <BarChart width={200} height={300} data={data}>
+        <Card style={{ flex: 1 }}>
+        <ResponsiveContainer width="100%" height={400}>
+            <BarChart width={200} height={300} data={data2}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Tooltip />
+              <XAxis dataKey="name" style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize:'10px' }}/>
+              <YAxis style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize:'10px' }} />
+              <Tooltip style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize:'10px' }}/>
               <Legend />
               <Bar dataKey="ventas" fill="#8884d8" />
             </BarChart>
+
           </ResponsiveContainer>
         </Card>
 
         {/* Segundo Gráfico */}
-        <Card style={{ width: '48%' }}>
-          <ResponsiveContainer width="100%" height={300}>
-            <RadialBarChart cx="50%" cy="50%" innerRadius="10%" outerRadius="80%" barSize={10} data={dataPie}>
-              <RadialBar minAngle={15} label={{ position: 'insideStart', fill: '#fff' }} background clockWise dataKey="uv" />
-              <Legend iconSize={10} layout="vertical" verticalAlign="middle" wrapperStyle={{ right: '20px', top: '20px' }} />
-            </RadialBarChart>
+        <Card style={{ flex: 1 }}>
+        <ResponsiveContainer width="100%" height="100%">
+        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={dataradar}>
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" />
+          <PolarRadiusAxis />
+          <Radar name="Mike" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+        </RadarChart>
+      </ResponsiveContainer>
+        </Card>
+
+      </div>
+      <div style={{ display: 'flex', marginTop: '15px', justifyContent: 'space-between', width: '100%' }}>
+      <Card style={{ flex: 1 }}>
+       
+        <ResponsiveContainer width="100%" height="100%">
+        <AreaChart
+          width={500}
+          height={400}
+          data={dataarea}
+          margin={{
+            top: 10,
+            right: 30,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize:'10px' }}/>
+          <YAxis style={{ fontFamily: 'Arial', fontWeight: 'bold', fontSize:'10px' }}/>
+          <Tooltip />
+          <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
+          <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+        </AreaChart>
+     
+          </ResponsiveContainer>
+        </Card>
+        <Card style={{ flex: 1 }}>
+          <ResponsiveContainer width="100%" height={400}>
+          <PieChart
+              series={[
+                {
+                  arcLabel: (item) => `${item.label} (${item.value})`,
+                  arcLabelMinAngle: 45,
+                  data,
+                },
+              ]}
+              sx={{
+                [`& .${pieArcLabelClasses.root}`]: {
+                  fill: 'white',
+                  fontWeight: 'bold',
+                },
+              }}
+              {...size}
+            />
           </ResponsiveContainer>
         </Card>
       </div>
-    {/* </div> */}
-    </BasicLayout>
+      </Card>
+      </Grid>
+        {/* </Grid> */}
+      </MDBox>
+    </PageLayout>
+
   );
 };
 
